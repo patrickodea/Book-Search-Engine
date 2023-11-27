78b73532500bf376b25d3c9f09da3f5cd3797d6c
Patrick O'Dea <po39dea@gmail.com>
2023-11-26 22:36:32 -0700

    updated auth.js to work with GraphQL

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -12,20 +12,21 @@ class AuthService {
   loggedIn() {
     // Checks if there is a saved token and it's still valid
     const token = this.getToken();
-    return !!token && !this.isTokenExpired(token); // handwaiving here
+    return token ? true : false; // handwaiving here
   }
 
   // check if token is expired
   isTokenExpired(token) {
-    try {
-      const decoded = decode(token);
-      if (decoded.exp < Date.now() / 1000) {
-        return true;
-      } else return false;
-    } catch (err) {
-      return false;
+    const decodedToken = jwt.decode(token);
+    const currentTime = Math.floor(Date.now() / 1000); // Get current time in seconds
+  
+    if (decodedToken.exp < currentTime) {
+      return true; // Token has expired
+    } else {
+      return false; // Token is still valid
     }
   }
+  
 
   getToken() {
     // Retrieves the user token from localStorage
