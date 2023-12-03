84f9df83aecb4f86ccfd20647dc5229678befd27
Patrick O'Dea <po39dea@gmail.com>
2023-12-03 14:46:12 -0700

    Added typedefs to schema

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,46 @@
+const typeDefs = `
+    type Book {
+        _id: ID!
+        authors: [String]
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        password: String!
+        savedBooks: [Book]
+    }
+
+    type Auth {
+        token: String!
+        user: User
+    }
+
+    input BookArrayInput {
+        authors: [String]
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type Query {
+        me(_id: String, username: String): User
+    }
+
+    type Mutation {
+        createUser(username: String!, email: String!, password String!): Auth
+        login(email: String!, password: String!): Auth
+        saveBook(content: BookarrayInput!): User
+        deleteBook(bookId: String!): User
+    }
+`;
+
+module.exports = typeDefs;
