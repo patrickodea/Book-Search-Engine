f07b7892ecf28738253bd653d5998d41f004c65e
Patrick O'Dea <po39dea@gmail.com>
2023-12-03 15:31:56 -0700

    Updated loginform component

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -2,8 +2,11 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+//import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+
+import { LOGIN } from "../utils/mutations";
+import { useMutation } from '@apollo/client';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
@@ -14,6 +17,8 @@ const LoginForm = () => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
   };
+
+  const [login, {loginError}] = useMutation(LOGIN);
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
@@ -26,22 +31,21 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+          const {data} = await login({
+            variables:{
+              email:userFormData.email,
+              password:userFormData.password
+            }
+          });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
+      console.log(loginError);
       setShowAlert(true);
     }
 
     setUserFormData({
-      username: '',
       email: '',
       password: '',
     });
